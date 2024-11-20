f109fa3a4e39d2d93f4fadb6d38b335a840ef858
VladyslavHlushchuk <hlushchukvlad03@gmail.com>
2024-11-20 13:54:18 +0200

    Fix some bug

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, ConflictException, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
   @Post('signup')
-  registerUser(@Body() createUserDto: CreateUserDto) {
-    return this.authService.registerUser(createUserDto);
+  async registerUser(@Body() createUserDto: CreateUserDto) {
+    try {
+      return await this.authService.registerUser(createUserDto);
+    } catch (error) {
+      throw new ConflictException(error.message || 'Unexpected error');
+    }
   }
 }
