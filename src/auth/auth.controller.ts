52f2126c7188347350358ca57cbd21719c4a452a
VladyslavHlushchuk <hlushchukvlad03@gmail.com>
2024-11-28 00:06:51 +0200

    Add JWT generation and API protection with JWT strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   ConflictException,
   Controller,
+  Get,
   Post,
   Request,
   UseGuards,
@@ -9,6 +10,7 @@ import {
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { LocalAuthGuard } from './guards/local-auth/local-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth/jwt-auth.guard';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -25,6 +27,14 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('signin')
   login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user.id, req.user.name);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('protected')
+  getAll(@Request() req) {
+    return {
+      messege: `Now you can access this protected API. this is your user ID: ${req.user.id}`,
+    };
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,10 +6,14 @@ import {
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { UserService } from 'src/user/user.service';
 import { verify } from 'argon2';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
   async registerUser(createUserDto: CreateUserDto) {
     const user = await this.userService.findByEmail(createUserDto.email);
     if (user) throw new ConflictException('User already exists!');
@@ -24,5 +28,32 @@ export class AuthService {
       throw new UnauthorizedException('Invalid Credentials!');
 
     return { id: user.id, name: user.name, role: user.role };
+  }
+
+  async login(userId: number, name: string) {
+    const { accessToken } = await this.generateTokens(userId);
+    return {
+      id: userId,
+      name: name,
+      accessToken,
+    };
+  }
+
+  async generateTokens(userId: number) {
+    const payload = { sub: userId };
+    const [accessToken] = await Promise.all([
+      this.jwtService.signAsync(payload),
+    ]);
+
+    return {
+      accessToken,
+    };
+  }
+
+  async validateJwtUser(userId: number) {
+    const user = await this.userService.findOne(userId);
+    if (!user) throw new UnauthorizedException('User not found!');
+    const currentUser = { id: user.id };
+    return currentUser;
   }
 }
